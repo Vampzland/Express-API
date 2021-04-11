0ed778d64cf59050124f175ef4cf9189350e491b
Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>
2021-04-11 11:06:57 -0400

    Added the book API

--- routes/book.js
+++ routes/book.js
@@ -9,7 +9,13 @@ const books = [
 
 
 router.get('/books',(req,res)=>{
-    res.status(200).send([1,2,3])
+    res.status(200).send(books)
+})
+
+router.get('/books/:id', (req,res)=>{
+    const { id } = req.params
+    const book = books.find(book => book.id === parseInt(id) )
+    res.status(200).send(book)
 })
 
 module.exports = router
