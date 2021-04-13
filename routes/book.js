11853ceec9a46f2ffa20d5353be8c960dbe8a597
Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>
2021-04-13 19:38:40 -0400

    Add the requested apis to the book router

--- routes/book.js
+++ routes/book.js
@@ -15,7 +15,28 @@ router.get('/books',(req,res)=>{
 router.get('/books/:id', (req,res)=>{
     const { id } = req.params
     const book = books.find(book => book.id === parseInt(id) )
+    if(!book) res.status(404).send('This book does not exist')
     res.status(200).send(book)
+})
+
+router.post('/books',(req,res)=>{
+    const book = {
+        id: books.length + 1,
+        name: req.body.name
+    }
+
+    books.push(book)
+    res.send(book)
+
+})
+
+router.put('/books/:id', (req,res)=>{
+    const {id} = req.params
+    const book = books.find(book=> book.id ===parseInt(id))
+    if(!book) res.status(404).send('This book does not exist')
+    book.name = req.body.name
+    res.send(book)
+
 })
 
 module.exports = router
